31e1463620204aaa61c973405a05fd87f12c89bb
bentok <bscopeland@gmail.com>
2021-02-05 22:39:40 -0500

    Make velocity immutable

--- FSharp.Scripting/Player.fs
+++ FSharp.Scripting/Player.fs
@@ -2,34 +2,36 @@ namespace FSharp.Player
 
 open Godot
 
+
+
 type PlayerFs() =
     inherit Area2D()
 
     [<Export>]
     let mutable Speed = 2.00f
 
-    let velocity = Vector2()
-
     let screensize = Vector2(480.00f, 720.00f)
 
     let velo (a, b) =
-            Vector2(a, b)
+        Vector2(a, b)
 
-    let getInput() =
-        velo(1.00f, 1.00f).Normalized() * Speed
-//        let mutable velocity = Vector2()
-//        if Input.IsActionPressed("ui_left") then velocity.x <- velocity.x - 1.00f
-//        if Input.IsActionPressed("ui_right") then velocity.x <- velocity.x + 1.00f
-//        if Input.IsActionPressed("ui_up") then velocity.y <- velocity.y - 1.00f
-//        if Input.IsActionPressed("ui_down") then velocity.x <- velocity.y + 1.00f
-//        if velocity.Length() > 0.00f then velocity = velocity.Normalized() * Speed
-//        else ignore 0
+    let veloX() =
+        if Input.IsActionPressed("ui_left") then -1.00f
+        elif Input.IsActionPressed("ui_right") then 1.00f
+        else 0.00f
+
+    let veloY() =
+        if Input.IsActionPressed("ui_up") then -1.00f
+        elif Input.IsActionPressed("ui_down") then 1.00f
+        else 0.00f
 
     override this._Ready() =
         ignore 0
 
     override this._Process(delta) =
-        let velocity = getInput()
+        let velocity = velo(veloX(), veloY()).Normalized() * Speed
         this.Position <- this.Position + delta * velocity
-//        this.Position <- (Mathf.Clamp(this.Position.x, 0.00f, screensize.x), Mathf.Clamp(this.Poition.y, 0.00f, screensize.y))
+        this.Position <- Vector2(
+            Mathf.Clamp(this.Position.x, 0.00f, screensize.x),
+            Mathf.Clamp(this.Position.y, 0.00f, screensize.y))
 
